ad8aa8ae932974d09c54868d23ea46d61b23b837
atomikolex <lex.atomiko@gmail.com>
2018-04-04 12:09:05 +0200

    1st try 3.0 syntax

---
+++ google-map-point.js
@@ -0,0 +1,36 @@
+import '../../@polymer/polymer/polymer.js';
+import { Polymer } from '../../@polymer/polymer/lib/legacy/polymer-fn.js';
+import '../google-apis/google-maps-api.js';
+
+Polymer({
+  is: 'google-map-point',
+
+  hostAttributes: {hidden: true},
+
+  properties: {
+    /**
+     * The point's longitude coordinate.
+     */
+    longitude: {
+      type: Number,
+      value: null
+    },
+
+    /**
+     * The point's latitude coordinate.
+     */
+    latitude: {
+      type: Number,
+      value: null
+    }
+  },
+
+  /**
+   * Returns the point as a Google Maps LatLng object.
+   *
+   * @return {google.maps.LatLng} The LatLng object.
+   */
+  getPosition: function() {
+    return new google.maps.LatLng(this.latitude, this.longitude);
+  }
+});
